17eaecdd371da0baa87f816023e46ce02dfbcbcd
Elva Friesen <efriesen@at-r8.21-school.ru>
2020-02-07 21:54:17 +0300

    choose ..

--- src/algo_suurbale.c
+++ src/algo_suurbale.c
@@ -6,7 +6,7 @@
 /*   By: efriesen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 18:21:05 by efriesen          #+#    #+#             */
-/*   Updated: 2020/02/06 19:23:49 by efriesen         ###   ########.fr       */
+/*   Updated: 2020/02/07 21:53:28 by efriesen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,64 @@ void	add_way(t_path *paths) // допиливать
 	}
 }
 
+void	remove_list_elem(t_edge **begin, t_edge *remove)
+{
+	if (remove == NULL)
+	{
+		printf("ERROR\n");
+		exit(0);
+	}
+
+	t_edge	*edge;
+	t_edge	*prev_edge;
+
+	edge = *begin;
+	prev_edge = NULL;
+
+	while (edge != remove)
+	{
+		prev_edge = edge;
+		edge = edge->next;
+	}
+	
+	if (prev_edge == NULL)
+		*begin = (*begin)->next;
+	else if (edge->next == NULL)
+		prev_edge->next = NULL;
+	else
+		prev_edge->next = edge->next;
+	free(edge);
+}
+
+void	remove_way(t_graph *graph)
+{
+	t_vertex	*vertex;
+	t_edge		*edge;
+	int			i;
+	int			j;
+
+	i = g_end;
+	while (i != -1)
+	{
+		vertex = graph->vertex;
+		j = -1;
+		while (++j < i)
+			vertex = vertex->next;
+		i = g_parent[i];
+		if (i != -1)
+		{
+			edge = vertex->edges;
+			while (edge)
+			{
+				if (edge->to == i)
+					break ;
+				edge = edge->next;
+			}
+			remove_list_elem(&vertex->edges, edge);
+		}
+	}
+}
+
 void	modific_cost(t_vertex *vertex)
 {
 	t_edge *edges;
@@ -41,7 +99,8 @@ void	modific_cost(t_vertex *vertex)
 		edges = vertex->edges;
 		while (edges)
 		{
-			edges->cost = edges->cost - g_dest[edges->to] + g_dest[vertex->room];
+			edges->cost = edges->cost
+				- g_dest[edges->to] + g_dest[vertex->room];
 			edges = edges->next;
 		}
 		vertex = vertex->next;
@@ -54,7 +113,7 @@ void	algo_suurbale(t_graph *graph)
 	paths->size = 0;
 
 	// while
-	print_graph(graph);
+	//print_graph(graph);
 
 	algo_dijkstra(graph);
 	print_way(graph->vector->names);
@@ -63,7 +122,14 @@ void	algo_suurbale(t_graph *graph)
 	
 	//delete edges before modific cost
 	
+	//print_edges(graph);
+
 	modific_cost(graph->vertex);
 
-	print_edges(graph);
+	remove_way(graph);
+
+	//print_edges(graph);
+
+	algo_dijkstra(graph);
+	print_way(graph->vector->names);
 }
