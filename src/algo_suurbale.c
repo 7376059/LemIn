7d66e3f424b5aadc8bb2cf47cd23ff002be3b87d
shchega <ilya@DESKTOP-QQ5K7MQ.localdomain>
2020-02-09 19:01:52 +0300

    suurbale

--- src/algo_suurbale.c
+++ src/algo_suurbale.c
@@ -18,18 +18,19 @@
  * 	printf("+");
  * */
 
-void	add_way(t_path *paths) // допиливать
+int		get_length(void)
 {
-	int len_way;
-	int ver;
-
-	len_way = 0;
-	ver = g_end;
-	while (ver != -1) 
+	int i;
+	int	j;
+	
+	i = -1;
+	j = g_end;
+	while (j != -1)
 	{
-		len_way++;
-		ver = g_parent[ver];
+		i++;
+		j = g_parent[j];
 	}
+	return (i);
 }
 
 void	remove_list_elem(t_edge **begin, t_edge *remove)
@@ -117,6 +118,7 @@ void	remove_way(t_graph *graph)
 
 void	modific_cost(t_vertex *vertex)
 {
+	
 	t_edge *edges;
 
 	while (vertex)
@@ -132,18 +134,55 @@ void	modific_cost(t_vertex *vertex)
 	}
 }
 
+void	init_path(t_path **path)
+{
+	int i;
+	
+	*path = (t_path*)malloc(sizeof(t_path));
+	(*path)->max_ways = 100;
+	(*path)->max_path = 100;
+	(*path)->ways_size = 0;
+	(*path)->ways = (int**)malloc(sizeof(int*) * 100);
+	i = -1;
+	while (++i < 100)
+		(*path)->ways[i] = (int*)malloc(sizeof(int) * 100);
+}
+
+void	add_way(t_path **path)
+{
+	int length;
+	int i;
+	
+	//printf("[%d]\n", get_length());
+	
+	if ((length = get_length()) > (*path)->max_path - 1)
+	;//extend_path(path);
+	if ((*path)->ways_size == (*path)->max_ways)
+	;//extend_ways(path);
+	
+	(*path)->ways[(*path)->ways_size][0] = length;
+	i = g_end;
+
+	while (length != 0)
+	{	
+		(*path)->ways[(*path)->ways_size][length--] = i;
+		i = g_parent[i];
+	}
+	
+	(*path)->ways_size++;
+}
+
 void	algo_suurbale(t_graph *graph)
 {
-	t_path *paths = (t_path*)malloc(sizeof(t_path));
-	paths->size = 0;
+	t_path *paths;
 
 	init_path(&paths);
 	// while
 	//print_graph(graph);
 
 	algo_dijkstra(graph);
-	
-	add_way  
+	add_way(&paths);
+	//add_way
 	
 	print_way(graph->vector->names);
 	
@@ -161,4 +200,19 @@ void	algo_suurbale(t_graph *graph)
 
 	algo_dijkstra(graph);
 	print_way(graph->vector->names);
+	add_way(&paths);
+	remove_way(graph);
+	algo_dijkstra(graph);
+	print_way(graph->vector->names);
+	add_way(&paths);
+	
+	for (int i = 0; i < 10; i++)
+	{
+		printf("[%d] ", paths->ways[i][0]);
+		for (int j = 1; j < paths->ways[i][0] + 1; j++)
+		{
+			printf("%d ", paths->ways[i][j]);
+		}
+		printf("\n");
+	}
 }
